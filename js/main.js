74ad587ce78b05d9e1162ac09d94d447de068112
Евгений <festProbe@gmail.com>
2021-07-01 19:53:45 +1100

    Исправлены названия функций переключения режимов активности страницы, в модуле переключения активности добавлены селекты фильтрации карты, изменен способ отображения интерактивных элементов у неактивных страниц

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 import { createAdvertisements } from './create-advertisment.js';
 import { createAdvertisementsMarkup } from './create-advertisements-markup.js';
 import { putAdvertisementInMarkup } from './put-advertisement-in-markup.js';
-import { formDisable, formEnable } from './disable-enable-forms.js';
+import { disableForm, enableForm } from './disable-enable-forms.js';
 
 
 const advertisements = createAdvertisements();
 const advertisementsMarkup = advertisements.map((card) => createAdvertisementsMarkup(card));
 putAdvertisementInMarkup(advertisementsMarkup[0]);
 
-formEnable();
-formDisable();
+enableForm();
+disableForm();
