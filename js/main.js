7f39ab135b35d9e5f1f1446ce28cd80d287f3e68
Евгений <festProbe@gmail.com>
2021-07-04 13:21:19 +1100

    Добавил синхронизацию по количеству комнат и количеству гостей, в модуле disable-enable-forms убрал hidden, добавил disabled, оформил константы с типами жилья в виде перечисления, в разметке перенес required в нужное место

--- js/main.js
+++ js/main.js
@@ -1,12 +1,7 @@
-import { createAdvertisements } from './create-advertisment.js';
-import { createAdvertisementsMarkup } from './create-advertisements-markup.js';
-import { putAdvertisementInMarkup } from './put-advertisement-in-markup.js';
-import { disableForm, enableForm } from './disable-enable-forms.js';
+//import { createAdvertisements } from './modules/create-advertisment.js';
+//import { createAdvertisementsMarkup } from './modules/create-advertisements-markup.js';
+//import { putAdvertisementInMarkup } from './modules/put-advertisement-in-markup.js';
 
 
-const advertisements = createAdvertisements();
-const advertisementsMarkup = advertisements.map((card) => createAdvertisementsMarkup(card));
-putAdvertisementInMarkup(advertisementsMarkup[0]);
-
-enableForm();
-disableForm();
+//const advertisements = createAdvertisements();
+//const advertisementsMarkup = advertisements.map((card) => createAdvertisementsMarkup(card));
